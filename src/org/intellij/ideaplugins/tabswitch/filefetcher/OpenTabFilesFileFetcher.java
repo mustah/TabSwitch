4e2d96ec1eb5425c0578ff204f47aa29f3ecd971
louyl <louyl@fenbi.com>
2015-10-19 17:27:25 +0800

    Ignore class file in Open Files list

--- src/org/intellij/ideaplugins/tabswitch/filefetcher/OpenTabFilesFileFetcher.java
+++ src/org/intellij/ideaplugins/tabswitch/filefetcher/OpenTabFilesFileFetcher.java
@@ -1,8 +1,5 @@
 package org.intellij.ideaplugins.tabswitch.filefetcher;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
@@ -10,10 +7,19 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ArrayUtil;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Creates a list of {@link VirtualFile} by fetching all the files that are open in tabs in current project.
  */
 public class OpenTabFilesFileFetcher implements FileFetcher<VirtualFile> {
+  private static Set<String> ignoredExtensions = new HashSet<>();
+  static {
+    ignoredExtensions.add("class");
+  }
 
   @Override
   public List<VirtualFile> getFiles(Project project) {
@@ -25,7 +31,8 @@ public class OpenTabFilesFileFetcher implements FileFetcher<VirtualFile> {
     List<VirtualFile> openFiles = new LinkedList<>();
     int editorTabLimit = UISettings.getInstance().EDITOR_TAB_LIMIT;
     for (VirtualFile file : recentFiles) {
-      if (openFiles.size() <= editorTabLimit && fileEditorManager.isFileOpen(file) && !openFiles.contains(file)) {
+      if (openFiles.size() <= editorTabLimit && fileEditorManager.isFileOpen(file) && !openFiles.contains(file)
+              && !ignoredExtensions.contains(file.getExtension())) {
         openFiles.add(file);
       }
     }
